b3390d712e9646a6ff1e9299935ccdf949a14ae4
Maurdekye <ncolaprete@gmail.com>
2020-08-22 17:38:53 -0400

    Changed mysql library, updated default config, changed test message

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const fsSync = require('fs');
-const fs = fsSync.promises;
+const fs = require('fs').promises;
+const mysql = require('promise-mysql');
 
 async function get_config(conf_path, default_config) {
-  let config = Object.assign({}, default_config);
+  let config = JSON.parse(JSON.stringify(default_config));
   try {
     let raw_cfg = await fs.readFile(conf_path);
     let loaded_cfg = JSON.parse(raw_cfg);
@@ -14,18 +14,27 @@ async function get_config(conf_path, default_config) {
   return config;
 }
 
-(async () => {
-  const app = express();
-  const config = await get_config('./config.json', {
-    port: 8080
-  });
+try{
+  (async () => {
+    const app = express();
+    const config = await get_config('./config.json', {
+      port: 8080,
+      mysql: {
+        host: 'localhost',
+        user: 'user',
+        password: 'password',
+        database: 'database'
+      }
+    });
 
-  app.get('/', (req, res) => {
-    res.send('Hello World!');
-  });
+    let mysql_connection = await mysql.createConnection(config.mysql);
 
-  app.listen(config.port, () => {
-    console.log(`Server listening on port ${config.port}`);
-  })
+    app.use(express.static("public_html"));
 
-})();
+    app.listen(config.port, () => {
+      console.log(`Server listening on port ${config.port}`);
+    })
+  })();
+} catch (err) {
+  throw err;
+}
